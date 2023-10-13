3df9cfec6a67d82b85b1b5ab4295ce3754f5e4f5
Alex52758 <volod902@mail.ru>
2023-10-13 15:16:54 +0300

    1

---
+++ 201-351_Ryzhenkov/mainwindow.cpp
@@ -0,0 +1,30 @@
+#include "mainwindow.h"
+#include "QCryptographicHash"
+#include "QFile"
+#include "QMessageBox"
+#include "QString"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::on_pushButton_clicked() {
+    QString pinCode = ui->Pin->text();
+
+
+
+
+    if ("1111" == pinCode) {  // сравнение расшифрованного текста с введенным пинкодом
+//        game window;
+//        window.setModal(true);
+        this->close();
+//        window.exec();
+    } else {
+        QMessageBox::warning(this, "Authotization", "Pin is incorrect");
+    }
+}
